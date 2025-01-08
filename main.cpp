a513610674f8e39322619372f56d2960b275e50c
255687 <255687@edu.p.lodz.pl>
2025-01-08 12:46:00 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int policz(int a,int b) {
+    return a+b;
+}
+int main() {
+    std::cout << "Hello World!\n" << std::endl;
+    std::cout << "2 + 2: " << policz(2,2) <<  std::endl;
+    return 0;
+}
